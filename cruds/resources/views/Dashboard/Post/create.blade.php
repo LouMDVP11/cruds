aa1f8f7a7d25db18c8cdc969085e36a79b06aac3
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2022-08-23 20:18:43 -0600

    Editado el form web

--- cruds/resources/views/Dashboard/Post/create.blade.php
+++ cruds/resources/views/Dashboard/Post/create.blade.php
@@ -6,32 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style=" background-color: #0E121F;">
-    <h1 style="text-align: center; background-color: #69C3FF;">Asistencia 23/08/2022</h1>
-    <h3 style="text-align: center; background-color:#121624; color:#CBCBCB">Lourdes del Valle - 202008019</h3>
-    <form action="{{route('post.store')}}" method="post" style="color:#F1FFFF">
+<body style="text-align: center; background-color: #0E121F;">
+    <h1 style="background-color: #69C3FF;">Asistencia 23/08/2022</h1>
+    <h3 style="background-color:#121624; color:#CBCBCB">Lourdes del Valle - 202008019</h3>
+    <form action="{{route('post.store')}}" method="post" style="color:#F1FFFF;">
         @csrf
-        
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-sm-6">
-                <label for="">Titulo</label>
-                <input type="text" name="title" style="text-align: center; background-color:#CBCBCB">
-            </div>
-            <div class="col-xs-12 col-sm-12 col-sm-6">
-                </div><label for="">Url Corta</label>
-                <input type="text" name="slug" style="text-align: center; background-color:#CBCBCB">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-sm-6">
-                <label for="">Contenido</label>
-                <textarea name="content" id="" cols="30" rows="10" style="text-align: center; background-color:#CBCBCB"></textarea>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-sm-6">
-                <label for="">Descripcion</label>
-                <textarea name="description" id="" cols="30" rows="10" style="text-align: center; background-color:#CBCBCB"></textarea>
-            </div>
-        </div>
+        <label for="">Titulo</label>
+        <input type="text" name="title" style="background-color:#CBCBCB">
+        </div><label for="">Url Corta</label>
+        <input type="text" name="slug" style="background-color:#CBCBCB">
+        <br><br>
+        <label for="">Contenido</label>
+        <textarea name="content" id="" cols="30" rows="10" style="background-color:#CBCBCB"></textarea>
+        <label for="">Descripcion</label>
+        <textarea name="description" id="" cols="30" rows="10" style="background-color:#CBCBCB"></textarea>
+        <br><br>
         <button type="submit">Enviar</button>
     </form>
 </body>
